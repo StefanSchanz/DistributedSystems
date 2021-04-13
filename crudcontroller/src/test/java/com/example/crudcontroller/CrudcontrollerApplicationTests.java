6ca41c2f851ad3bc92dfe0f16b12db4e9a5c1373
StefanSchanz <Stefan.Schanz@stud.hs-esslingen.de>
2021-04-13 17:11:56 +0200

    Appended the Class in the test

--- crudcontroller/src/test/java/com/example/crudcontroller/CrudcontrollerApplicationTests.java
+++ crudcontroller/src/test/java/com/example/crudcontroller/CrudcontrollerApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.crudcontroller;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {com.example.crudcontroller.CrudcontrollerApplication.class})
 class CrudcontrollerApplicationTests {
 
     @Test
