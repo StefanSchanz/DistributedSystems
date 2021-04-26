e8ad772e2e3c9c7ffe124571d76577de6edca0f0
StefanSchanz <Stefan.Schanz@stud.hs-esslingen.de>
2021-04-26 16:52:30 +0200

    Changed some things ;)

--- persistence_example/src/main/java/com/example/persistence_example/JPAController.java
+++ persistence_example/src/main/java/com/example/persistence_example/JPAController.java
@@ -27,16 +27,14 @@ public class JPAController {
 
         stringRepository.findAll().forEach(string -> listofStrings.add(string.getString()));
 
-        logger.info("Strings being returned: ", listOfStrings.toString());
+        logger.info("Strings being returned: ", listofStrings.toString());
 
-        return listOfStrings.toString();
+        return listofStrings.toString();
     }
 
     @PostMapping("/strings/{newString}")
     public String addNewString(@PathVariable String newString){
         logger.error("Strings to add: {}", newString);
-
-        stringRepository.save();
 
         List<String> listofStrings = new ArrayList<String>();
         logger.debug("Current list of strings: {}", getAllStrings());
@@ -46,16 +44,16 @@ public class JPAController {
     @PutMapping("/strings/{oldString}/{newString}")
     public String updateString(@PathVariable String oldString, @PathVariable String newString){
         logger.info("Strings to delete: ", oldString);
-        listOfStrings.remove(oldString);
+        // listOfStrings.remove(oldString);
         logger.info("Strings to add: ", newString);
-        listOfStrings.add(newString);
+        // listOfStrings.add(newString);
         return newString + " has been added to the list";
     }
 
     @DeleteMapping("/strings/{stringToDelete}")
     public String deleteString(@PathVariable String stringToDelete){
         logger.info("Strings to delete: ", stringToDelete);
-        listOfStrings.remove(stringToDelete);
+        // listOfStrings.remove(stringToDelete);
         return stringToDelete + " has most likely removed from the list";
     }
 
