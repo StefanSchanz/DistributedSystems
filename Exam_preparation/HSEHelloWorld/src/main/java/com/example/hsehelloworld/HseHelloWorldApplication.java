5d7dfdc85bbb1a67581f66e1b41fc9e32d392cb1
StefanSchanz <Stefan.Schanz@stud.hs-esslingen.de>
2021-06-18 16:18:01 +0200

    Started the Preparation of the Exam

---
+++ Exam_preparation/HSEHelloWorld/src/main/java/com/example/hsehelloworld/HseHelloWorldApplication.java
@@ -0,0 +1,21 @@
+package com.example.hsehelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController     // Need that to Define the Handler for Rest Calls
+public class HseHelloWorldApplication {
+
+    @GetMapping             // GetMapping on Functions that should Used with GETs!
+    public String sayHello(){   // Function that responds to a GET Call on the Webserver!
+        return "Hello Esslingen, Summer Semester 2021";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HseHelloWorldApplication.class, args);
+    }
+
+}
