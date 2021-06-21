da23a7bbea943de2b20da63b27880b4d167d080e
StefanSchanz <Stefan.Schanz@stud.hs-esslingen.de>
2021-06-21 10:24:27 +0200

    Created the persistence example exercise for the exam preparation.

---
+++ Exam_preparation/persistence/src/main/java/com/example/persistence/JPAController.java
@@ -0,0 +1,66 @@
+package com.example.persistence;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class JPAController {
+
+    @Autowired
+    private StringRepository stringRepository;
+    private Logger logger = LoggerFactory.getLogger(JPAController.class);
+
+    // Get Mapping for URL /strings
+    @GetMapping("/strings")
+    public String readAllStrings(){
+        List<String> listofStrings = new ArrayList<>();
+
+        for(StringEntity string: stringRepository.findAll()) {
+            listofStrings.add(string.getString());
+        }
+
+        logger.info("Strings being returned {}", listofStrings);
+        return listofStrings.toString();
+    }
+
+    // Post Mapping for...
+    @PostMapping("/strings/{newString}")
+    public String createString(@PathVariable String newString){
+        logger.error("String to add: {}", newString);
+
+        stringRepository.save(new StringEntity(newString));
+
+        logger.debug("Current list of Strings{}", readAllStrings());
+        return newString + " has been added to the list";
+    }
+
+    // Put Mapping...
+    /*
+    @PutMapping("/strings/{oldString}/{newString}")
+    public String updateString(@PathVariable String oldString, @PathVariable String newString){
+        for (int i = 0; i < stringList.size(); i++){
+            if(oldString.equals(stringList.get(i))){
+                String tmp = stringList.get(i);
+                stringList.set(i, newString);
+                return "sucessfully changed " + tmp + " to " + newString;
+            }
+        }
+        return "String " + oldString + " was not found!";
+    }
+
+    // Delete Mapping
+    @DeleteMapping("/strings/{stringToDelete}")
+    public String deleteString(@PathVariable String stringToDelete){
+        logger.info("String to delete: {}", stringToDelete);
+        stringList.remove(stringToDelete);
+        logger.debug("Current list of strings {}", stringList);
+        return stringToDelete + " has been removed from the internal list!";
+    }
+    */
+}
---
+++ Exam_preparation/persistence/src/main/java/com/example/persistence/StringEntity.java
@@ -0,0 +1,20 @@
+package com.example.persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class StringEntity {
+    @Id
+    private String string;
+
+    public StringEntity(){}
+
+    public StringEntity(String string){
+        this.string = string;
+    }
+
+    public String getString(){
+        return this.string;
+    }
+}
